968966ec61bef7e3a85c9c473ad2129bfc4ccb80
FranRubio23 <francesc.rubio@sarria.salesians.cat>
2024-12-04 16:29:08 +0100

    Modificacion Custom Control TextBox y Modificacion FormPrincipal
    Se ha añadido el boton para desplegar o esconder el menu y se ha modificado el textbox custom por un campo erroneo. Falta revision de una parte del codigo del custom control.

--- MainFormSC/frmPrincipal.Designer.cs
+++ MainFormSC/frmPrincipal.Designer.cs
@@ -33,6 +33,8 @@ namespace MainFormSC
             this.menuTransicio = new System.Windows.Forms.Timer(this.components);
             this.pnlMenu = new System.Windows.Forms.Panel();
             this.pnlSuperior = new System.Windows.Forms.Panel();
+            this.btnDesplegable = new System.Windows.Forms.Button();
+            this.pnlSuperior.SuspendLayout();
             this.SuspendLayout();
             // 
             // pnlMenu
@@ -46,10 +48,21 @@ namespace MainFormSC
             // pnlSuperior
             // 
             this.pnlSuperior.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.pnlSuperior.Controls.Add(this.btnDesplegable);
             this.pnlSuperior.Location = new System.Drawing.Point(0, 0);
             this.pnlSuperior.Name = "pnlSuperior";
             this.pnlSuperior.Size = new System.Drawing.Size(1192, 75);
             this.pnlSuperior.TabIndex = 1;
+            // 
+            // btnDesplegable
+            // 
+            this.btnDesplegable.Location = new System.Drawing.Point(3, 4);
+            this.btnDesplegable.Name = "btnDesplegable";
+            this.btnDesplegable.Size = new System.Drawing.Size(110, 68);
+            this.btnDesplegable.TabIndex = 3;
+            this.btnDesplegable.Text = "buttonMenu";
+            this.btnDesplegable.UseVisualStyleBackColor = true;
+            this.btnDesplegable.Click += new System.EventHandler(this.btnDesplegable_Click);
             // 
             // frmPrincipal
             // 
@@ -61,6 +74,7 @@ namespace MainFormSC
             this.IsMdiContainer = true;
             this.Name = "frmPrincipal";
             this.Text = "frmPrincipal";
+            this.pnlSuperior.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
@@ -70,5 +84,6 @@ namespace MainFormSC
         private System.Windows.Forms.Timer menuTransicio;
         private System.Windows.Forms.Panel pnlMenu;
         private System.Windows.Forms.Panel pnlSuperior;
+        private System.Windows.Forms.Button btnDesplegable;
     }
 }
